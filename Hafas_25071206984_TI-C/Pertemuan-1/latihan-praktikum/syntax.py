ee950c29b3b0a5dd8a52f8d6bd0fa53373f85745
hafasrevan <hafastreifan@gmail.com>
2026-01-31 20:49:40 +0700

    Add files via upload

---
+++ Hafas_25071206984_TI-C/Pertemuan-1/latihan-praktikum/syntax.py
@@ -0,0 +1,32 @@
+#=============================================================
+# Pertemuan 1 - Syntax Python 
+#=============================================================
+
+#A. Statement 
+#Adalah  perintah yang dijalankan oleh python, contoh :
+
+print ("Hello world!") #Ini adalah contoh statement sederhana
+
+#B. Syntax
+#Adalah aturan dalam  menulis kode Python, contoh :
+
+if 5 > 2:
+    print ("five is greater than two") #Contoh syntax yang benar, Karna ada jarak antara IF dan Print di bawahnya
+
+if 10 > 2:
+print ("ten is greater than two") #Contoh syntax yang salah dan menghasilkan error
+
+#C. Input & Output
+#Input adalah pemasukan data yang kita masukkan ke program.
+#Output adalah hasil olahan program yang sesuai dengan permintaan pengguna.
+
+#Contoh :
+
+#input yang dimasukkan
+nama = "Reifan" #Kita memasukkan data dalam variabel nama,yang dimana nama nya adalah Reifan
+
+print ("Perkenalkan saya", nama) #(Statement)
+
+#Output yang akan dikeluarkan adalah 
+#Perkenalkan saya Reifan
+
