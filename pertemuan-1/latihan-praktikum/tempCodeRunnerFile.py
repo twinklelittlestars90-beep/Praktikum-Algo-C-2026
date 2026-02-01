af437755cd2033261700166a1e09128d5fa9c339
yohanes-Tambunan <yohanes.doly4585@student.unri.ac.id>
2026-02-01 17:45:58 +0700

    comments.py

--- pertemuan-1/latihan-praktikum/tempCodeRunnerFile.py
+++ pertemuan-1/latihan-praktikum/tempCodeRunnerFile.py
@@ -1,18 +1,3 @@
-#1. cara menampilkan teks pada python
-#kita dapat menggunakan fungsi print () untuk menampilkan teks atau nilai keluaran
-print("hello world!")
-print("hello mars!")
-#teks harus berada diantara simbol "" ataupun simbol '
-print("makanan")
-print('makanan')
-
-#jika kita ingin menampilkan nilai keluaran sebaris,kita bisa menggunakan "end"
-print("aku suka kamu", end=" ")
-print("tapi boong")
-
-#2. cara menampilkan angka pada python
-#kita dapat menggunakan fungsi print () untuk menampilkan angka.
-print(3)
-print(3)
-print(3 + 3)
-
+#komen digunakan untuk menjelaskan kode pada python
+#komen dimulai dari simbol tanda pagar
+print("halo dunia") #ini adalah komen
